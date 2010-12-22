3b681c010c1511d3130ae3141796722e90a77bff
fogus <mefogus@gmail.com>
2010-12-22 16:22:43 -0500

    added eval-map

--- src/evalive/core.clj
+++ src/evalive/core.clj
@@ -1,1 +1,11 @@
 (ns evalive.core)
+
+(defn eval-map [ctx expr]
+  (eval
+   `(let [~@(mapcat (fn [[k v]] [k `'~v])
+                    ctx)]
+      ~expr)))
+
+(comment
+  (eval-map '{a 10} '(* a a)))
+
