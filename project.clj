a9c2caffc5b9a9c162ef5f764e9247e4273d6f83
fogus <mefogus@gmail.com>
2011-01-13 11:28:32 -0500

    Restructuring the eval functions into a protocol and added util functions

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
-(defproject evalive "1.0.0-SNAPSHOT"
+(defproject evalive "0.1.0"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0-beta1"]
-                 [org.clojure/clojure-contrib "1.2.0-beta1"]])
+                 [org.clojure/clojure-contrib "1.2.0-beta1"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
+                     [jline "0.9.94"]
+                     [swank-clojure "1.2.1"]
+                     [marginalia "0.3.0"]])
--- src/evalive/core.clj
+++ src/evalive/core.clj
@@ -1,11 +1,93 @@
 (ns evalive.core)
 
-(defn eval-map [ctx expr]
-  (eval
-   `(let [~@(mapcat (fn [[k v]] [k `'~v])
-                    ctx)]
-      ~expr)))
+(defn- map-eo
+  [f & colls]
+  (apply map
+         #(%1 %&)
+         (cycle [first #(apply f %)])
+         colls))
+
+(def compile-time-lookup #(do `'~%))
+
+;; # Public API
+
+(defmacro lexical-context
+  []
+  (let [symbols (keys &env)]
+    (zipmap (map (fn [sym] `(quote ~sym))
+                 symbols)
+            symbols)))
+
+(defprotocol Evil
+  (evil [this form]))
+
+(extend-type java.util.Map
+  Evil
+  (evil [this form]
+    (eval
+     `(let [~@(mapcat (fn [[k v]] [k `'~v])
+                      this)]
+        ~form))))
+
+(extend-type java.util.List
+  Evil
+  (evil [this form]
+    (eval
+     `(let [~@(map-eo compile-time-lookup this)]
+        ~form))))
+
+(extend-type (Class/forName "[Ljava.lang.Object;")
+  Evil
+  (evil [this form]
+    (evil (seq this) form)))
+
 
 (comment
-  (eval-map '{a 10} '(* a a)))
+  (evil '{a 100 b 2} '(* a b))
+  (evil '[a 100 b 22] '(* a b))
+  (evil (to-array '[a 200 b 5]) '(* a b))
 
+  (eval 'x)
+; blowed up
+
+  (def x 1)
+  (eval 'x)
+;=> 1
+
+  (binding [x 100]
+    (eval 'x))
+;=> 100
+
+  (let [x 200]
+    (eval 'x))
+
+  (defn foo []
+    y)
+; will not compile
+
+  (defmacro foo []
+    'y)
+
+  (macroexpand '(foo))
+  (foo)
+; blowed up
+
+  (def y 42)
+  (let [y 100000]
+    (foo))
+
+  (defn foo [] y)
+
+  (let [y 100000]
+    (foo))
+
+  (ns foo)
+  (eval 'x)
+; blowed up
+
+  (eval 'user/x)
+; 1
+
+  (binding [user/x 12]
+    (eval 'user/x))
+)
