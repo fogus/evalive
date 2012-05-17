ed8e58630c9fdb0abe44692ed0c6196a775435d9
fogus <mefogus@gmail.com>
2012-05-17 09:51:27 -0400

    Added test.gen

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                      [jline "0.9.94"]
                      [swank-clojure "1.4.0"]
-                     [lein-marginalia "0.7.0"]])
+                     [lein-marginalia "0.7.0"]
+                     [lein-generative "0.1.4.0-SNAPSHOT"]]
+  :generative-path "test/")
