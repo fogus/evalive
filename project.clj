e8e21069fe2303bc0f86478f4c203fcc037cc583
fogus <mefogus@gmail.com>
2012-06-02 09:21:32 -0400

    Upgrading to clj 1.4

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject evalive "1.1.0"
   :description "Various eval functions and macros for Clojure"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                      [jline "0.9.94"]
                      [swank-clojure "1.4.0"]
