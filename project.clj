aac80d416d53516fbbf407695c53be3e0e72e2b2
fogus <mefogus@gmail.com>
2012-06-20 08:04:13 -0400

    updating to clj15

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject evalive "1.1.0"
   :description "Various eval functions and macros for Clojure"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.0-alpha1"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                      [jline "0.9.94"]
                      [swank-clojure "1.4.0"]
-                     [lein-marginalia "0.7.0"]
+                     [lein-marginalia "0.8.0-SNAPSHOT"]
                      [lein-generative "0.1.4.0-SNAPSHOT"]]
   :generative-path "test/")
